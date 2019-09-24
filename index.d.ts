d0c06527efb9968503b354e0f1ce15c418d85af9
Caleb Meredith <calebmeredith8@gmail.com>
2019-09-24 14:36:33 -0700

    Totally rewrite the types for `create-react-class`.

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,54 @@
-// Type definitions for create-react-class 15.6
-// Project: https://facebook.github.io/react/
-// Definitions by: John Gozde <https://github.com/jgoz>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
+import React from 'react';
+import * as PropTypes from 'prop-types';
 
-import { ComponentSpec, ClassicComponentClass } from "react";
+// A strongly typed version of `createReactClass()` which aims for compatibility with Flow’s
+// support for `createReactClass()`. We make various type safety compromises to ease the use of
+// `createReactClass()` at Airtable.
+export default function createReactClass<
+    // We don’t type check known React methods like `render()` or `componentDidUpdate()`.
+    //
+    // 1. If we put them in the supertype, here, and they fail to type check you get strange
+    //    unrelated errors.
+    // 2. If we add them to the intersection of `Spec & ThisType<...>` below then TypeScript
+    //    won’t be able to fully infer `Spec`.
+    //
+    // The tradeoff we make here is no type checking for known methods so that we get complete
+    // type checking for custom methods.
+    Spec extends {propTypes?: unknown, getInitialState?: unknown}
+>(
+    spec: Spec & ThisType<ComponentInstance<Spec>>,
+): React.ComponentType<
+    // Infer the type of our component’s props from the `React.createClass` spec.
+    PropTypes.InferProps<Spec['propTypes']> &
 
-declare namespace createReactClass {}
-declare function createReactClass<P, S>(spec: ComponentSpec<P, S>): ClassicComponentClass<P>;
+    // Make sure `ref` has the correct type.
+    {ref?: React.Ref<ComponentInstance<Spec>>}
+>;
 
-export as namespace createReactClass;
-export = createReactClass;
+type ComponentInstance<Spec extends {propTypes?: unknown, getInitialState?: unknown}> =
+    // All methods in the spec are available with `this`.
+    Spec &
+
+    // Infer the types of props and state from our spec.
+    ComponentInstanceBase<
+        PropTypes.InferProps<Spec['propTypes']>,
+        InferState<Spec>
+    > &
+
+    // Any properties which weren’t handled by the above are treated as `any`. This is
+    // required for Airtable’s mixin support since `EventListenerMixin` adds generated
+    // methods to the class.
+    {[key: string]: any};
+
+type InferState<Spec extends {getInitialState?: unknown}> =
+    Spec['getInitialState'] extends (...args: any) => infer State ? State : {};
+
+type ComponentInstanceBase<P, S> = {
+    props: P,
+    state: S,
+
+    setState<K extends keyof S>(
+        state: ((prevState: Readonly<S>, props: Readonly<P>) => (Pick<S, K> | S | null)) | (Pick<S, K> | S | null),
+        callback?: () => void
+    ): void;
+};
